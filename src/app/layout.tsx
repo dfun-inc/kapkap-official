1efe2ba3a94fc846e8a8dba1d488a5170e60908e
todamoonDev <dev@star.meme>
2025-11-18 17:13:08 +0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,23 +38,19 @@ export default function RootLayout({
     <html lang={locale == 'en' ? 'en' : 'zh-CN'}>
       <head>
         <title>KapKap - Al-driven Web3 Attention Economy and Value Distribution Platform</title>
-        <script dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-N2P38SKS');`}} 
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-W2W6D5TFRQ" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-W2W6D5TFRQ');
+            `,
+          }}
         />
       </head>
       <body>
-        <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-N2P38SKS"
-            height="0" 
-            width="0" 
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
         <AppProvider>
           <EvmProvider>
             <TonConnectUIProvider manifestUrl="https://cdn.kapkap.io/mainifest.json">
